b1662759cff1fd522a241de2fe8e9267d6140748
huangqihao7 <448252250@qq.com>
2019-09-23 16:05:03 +0800

    个人练手

--- zookeeper/src/main/java/com/qihao/ZookeeperApplication.java
+++ zookeeper/src/main/java/com/qihao/ZookeeperApplication.java
@@ -2,8 +2,10 @@ package com.qihao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZookeeperApplication {
 
     public static void main(String[] args) {
